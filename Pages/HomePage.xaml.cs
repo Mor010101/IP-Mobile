dd24f6894d27f1fa7775a6da6a217a67e40fdee6
Mădălin-Florin GOIAN <79133273+MadalinGOIAN@users.noreply.github.com>
2023-05-10 00:03:21 +0300

    14/feature-create-home-page
    Added Animations + Refactorings

--- Pages/HomePage.xaml.cs
+++ Pages/HomePage.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.Maui.Controls;
+
 namespace Mobile_IP;
 
 public partial class HomePage : ContentPage
@@ -7,19 +9,48 @@ public partial class HomePage : ContentPage
 		InitializeComponent();
 	}
 
-    private void OnEditImageButton_Clicked(object sender, EventArgs e)
-    {
-        setMaxValuesFrame.IsEnabled = true;
-        setMaxValuesFrame.IsVisible = true;
+	private async void OnEditImageButton_Clicked(object sender, EventArgs e)
+	{
+		SetEntryTextToLabelText();
+		ActivateSetMaxValuesFrame();
 
-        maxValuesFrame.IsEnabled = false;
-    }
+		maxValuesFrame.IsEnabled = false;
 
-    private void OnSaveButton_Clicked(object sender, EventArgs e)
-    {
-        setMaxValuesFrame.IsEnabled = false;
-        setMaxValuesFrame.IsVisible = false;
+		setMaxValuesFrame.Opacity = 0;
+		await Microsoft.Maui.Controls.ViewExtensions.FadeTo(setMaxValuesFrame, 1);
+	}
+
+	private void SetEntryTextToLabelText()
+	{
+		bpmMaxValueEntry.Text = bpmMaxValueLabel.Text;
+		tempMaxValueEntry.Text = tempMaxValueLabel.Text.Substring(0, tempMaxValueLabel.Text.Length - 1);
+	}
+
+	private void ActivateSetMaxValuesFrame()
+	{
+		setMaxValuesFrame.IsEnabled = true;
+		setMaxValuesFrame.IsVisible = true;
+	}
+
+	private async void OnSaveButton_Clicked(object sender, EventArgs e)
+	{
+		SetLabelTextToEntryText();
 
         maxValuesFrame.IsEnabled = true;
+
+		await Microsoft.Maui.Controls.ViewExtensions.FadeTo(setMaxValuesFrame, 0);
+		DeactivateSetMaxValuesFrame();
+	}
+
+	private void SetLabelTextToEntryText()
+	{
+        bpmMaxValueLabel.Text = bpmMaxValueEntry.Text;
+        tempMaxValueLabel.Text = tempMaxValueEntry.Text + "°";
     }
+
+	private void DeactivateSetMaxValuesFrame()
+	{
+		setMaxValuesFrame.IsEnabled = false;
+		setMaxValuesFrame.IsVisible = false;
+	}
 }
