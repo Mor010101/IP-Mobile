55296f5c1ee5185c1900d5b5127a4f65ec71edb5
Razvan <razvanionescu26@yahoo.com>
2023-05-11 12:02:12 +0300

    17/feature-create-calendar
    Implemented skeleton calendar

---
+++ ViewModel/CalendarViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XCalendar.Core.Models;
+
+namespace Mobile_IP.ViewModel
+{
+    class CalendarViewModel
+    {
+        public Calendar<CalendarDay> MyCalendar { get; set; } = new Calendar<CalendarDay>();
+    }
+}
