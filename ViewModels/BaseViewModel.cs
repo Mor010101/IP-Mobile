d5305fdbb9c96fbe5890300d52877513c2bbb1f0
Mădălin-Florin GOIAN <79133273+MadalinGOIAN@users.noreply.github.com>
2023-05-16 19:19:50 +0300

    17/feature-create-calendar-page
    Added Style

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_IP.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Methods
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
--- ViewModels/CalendarViewModel.cs
+++ ViewModels/CalendarViewModel.cs
@@ -7,14 +7,38 @@ using XCalendar.Core.Models;
 using XCalendar.Core.Extensions;
 using System.Windows.Input;
 using XCalendar.Maui.Views;
+using Mobile_IP.ViewModels;
+using XCalendar.Core.Enums;
 
 namespace Mobile_IP.ViewModel
 {
-    class CalendarViewModel
+    public class CalendarViewModel : BaseViewModel
     {
-        public Calendar<CalendarDay> MyCalendar { get; set; } = new Calendar<CalendarDay>();
+        public Calendar<CalendarDay> MyCalendar { get; set; } = new Calendar<CalendarDay>()
+        {
+            NavigatedDate = DateTime.Today,
+            NavigationLowerBound = DateTime.Today.AddYears(-2),
+            NavigationUpperBound = DateTime.Today.AddYears(2),
+            StartOfWeek = DayOfWeek.Monday,
+            SelectionAction = SelectionAction.Modify,
+            NavigationLoopMode = NavigationLoopMode.LoopMinimumAndMaximum,
+            SelectionType = SelectionType.Single,
+            PageStartMode = PageStartMode.FirstDayOfMonth,
+            Rows = 2,
+            AutoRows = true,
+            AutoRowsIsConsistent = true,
+            TodayDate = DateTime.Today
+        };
+        public double DaysViewHeightRequest { get; set; } = 440;
+        public double DayNamesHeightRequest { get; set; } = 25;
+        public double NavigationHeightRequest { get; set; } = 50;
+        public double DayHeightRequest { get; set; } = 45;
+        public double DayWidthRequest { get; set; } = 45;
+        public bool DayAutoSetStyleBasedOnDayState { get; set; } = true;
+        public int ForwardsNavigationAmount { get; set; } = 1;
+        public int BackwardsNavigationAmount { get; set; } = -1;
+
         public ICommand NavigateCalendarCommand { get; set; }
-        public DaysView myDays { get; set; } = new DaysView();
 
         public CalendarViewModel()
         {
