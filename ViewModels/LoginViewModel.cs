2dc9d0be30c5a0bf51550c042fab9ec93541ddbd
Tudor Moroșan <tudor.morosan@student.upt.ro>
2023-06-05 16:52:59 +0300

    publish branch

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Nancy.Json;
 
 namespace Mobile_IP.ViewModels
 {
@@ -12,6 +14,10 @@ namespace Mobile_IP.ViewModels
     {
         public Action DisplayInvalidLoginPrompt;
         public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("http://34.140.195.43:80/api/Auth");
+        StreamWriter streamWriter;
+
+
         private string email;
         public string Email
         { get { return email; } 
@@ -36,17 +42,38 @@ namespace Mobile_IP.ViewModels
         public LoginViewModel()
         {
             SubmitCommand = new Command(OnSubmit);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
         }
         public void OnSubmit()
         {
-            if (email != "laurentiu" || password != "macaronar")
+            var values = new Dictionary<string, string>
+            {
+                  { "email", email},
+                  { "password", password}
+            };
+
+            using (streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = new JavaScriptSerializer().Serialize(values);
+                streamWriter.Write(json);
+                streamWriter.Flush();
+            }
+
+            try
+            {
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                if(httpResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    Application.Current.MainPage = new AppShell();
+                }
+            }
+            catch(Exception e)
             {
                 DisplayInvalidLoginPrompt();
             }
-            else
-            {
-                Application.Current.MainPage = new AppShell();
-            }
+
         }
 
     }
