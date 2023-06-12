bd90a192aa249dec597f41d2b36359299fc84825
Mădălin-Florin GOIAN <79133273+MadalinGOIAN@users.noreply.github.com>
2023-06-12 19:46:29 +0300

    28/feature-add-line-chart-for-bpm-graph

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,5 @@
 ﻿using CommunityToolkit.Maui;
-using CommunityToolkit.Maui;
+using Syncfusion.Maui.Core.Hosting;
 
 namespace Mobile_IP;
 public static class MauiProgram
@@ -7,7 +7,11 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+        builder
+            .UseMauiApp<App>()
+            .ConfigureSyncfusionCore()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
         {
             fonts.AddFont("Poppins-Regular.ttf", "PoppinsRegular");
             fonts.AddFont("Poppins-Semibold.ttf", "PoppinsSemibold");
